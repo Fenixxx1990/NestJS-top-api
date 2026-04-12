a78f0382379c437690d7be0d6d64a894b4f1d965
Fenix <fenixxx1990@yandex.ru>
2026-04-13 00:24:39 +0400

    images get set

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { ReviewModule } from "./review/review.module";
 import { ConfigModule, ConfigService } from "@nestjs/config";
 import { MongooseModule } from "@nestjs/mongoose/dist";
 import { getMongoConfig } from "./configs/mongo.config";
+import { FilesModule } from './files/files.module';
 
 @Module({
   imports: [
@@ -23,6 +24,7 @@ import { getMongoConfig } from "./configs/mongo.config";
     TopPageModule,
     ProductModule,
     ReviewModule,
+    FilesModule,
   ],
   controllers: [AppController],
   providers: [AppService],
