5e1578c6e47a011ad9c707c580219cfdd19aca22
Fenix <fenixxx1990@yandex.ru>
2026-04-13 23:32:09 +0400

    api hh

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,10 +12,12 @@ import { FilesModule } from "./files/files.module";
 import { SitemapModule } from "./sitemap/sitemap.module";
 import { TelegramModule } from "./telegram/telegram.module";
 import { getTelegramConfig } from "./configs/telegram.config";
-import { HhModule } from './hh/hh.module';
+import { HhModule } from "./hh/hh.module";
+import { ScheduleModule } from "@nestjs/schedule";
 
 @Module({
   imports: [
+    ScheduleModule.forRoot(),
     ConfigModule.forRoot({
       isGlobal: true,
     }),
